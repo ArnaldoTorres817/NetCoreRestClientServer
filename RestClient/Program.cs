311ec81d7f36d7d790e72ca34b943a6da00194f7
Arnaldo Torres <torres.arnaldo817@gmail.com>
2020-10-25 17:04:13 -0400

    feat: version 1.0

---
+++ RestClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RestClient
+{
+    class Program
+    {
+        static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            // client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+            
+            try
+            {
+                // Analizar gramaticalmente el URI
+                Uri requestUri = new Uri(args[0]);
+                // Hacer la peticion
+                HttpResponseMessage response = await client.GetAsync(requestUri);
+                // Tirar excepcion si el status code no es exitoso
+                response.EnsureSuccessStatusCode();
+                // Peticion exitosa
+                Console.WriteLine(response.StatusCode);
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+            catch (UriFormatException e) {
+                Console.WriteLine(e.Message);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ RestServer/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+
+namespace RestServer
+{
+    class Program
+    {
+        static readonly HttpListener listener = new HttpListener();
+
+        static readonly JsonSerializerOptions jsonSerializerOptions = new JsonSerializerOptions
+        {
+            AllowTrailingCommas = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            PropertyNameCaseInsensitive = true
+        };
+
+        static void Main(string[] args)
+        {
+            // Handle CTRL+C for shutting down the server cleanly.
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                Console.WriteLine("Received SIGINT signal. Shutting down the server.");
+                listener.Stop();
+                listener.Close();
+                Environment.Exit(0);
+            };
+            Uri uri = BuildUri();
+            StartListener(uri);
+        }
+
+        static Uri BuildUri()
+        {
+            UriBuilder uriBuilder = new UriBuilder();
+            uriBuilder.Host = "localhost";
+            uriBuilder.Port = 5000;
+            return uriBuilder.Uri;
+        }
+
+        static void StartListener(Uri uri)
+        {
+            // Add uri to listener prefixes
+            listener.Prefixes.Add(uri.AbsoluteUri);
+
+            // Start listening...
+            listener.Start();
+            Console.WriteLine("Server is listening...");
+
+            HttpListenerContext context;
+            do
+            {
+                try
+                {
+                    context = listener.GetContext();
+                    Console.WriteLine("Received request.");
+                    HandleContext(context);
+                }
+                catch (HttpListenerException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            } while (listener.IsListening);
+        }
+
+        static void HandleContext(HttpListenerContext context)
+        {
+            if (context.Request.Url.AbsolutePath.Equals("/"))
+            {
+                SendReponse(context.Response);
+            }
+            else
+            {
+                SendNotFound(context.Response);
+            }
+        }
+
+        static void SendReponse(HttpListenerResponse response)
+        {   
+            // TODO: Error handling and finally clause for closing output stream
+            response.StatusCode = 200;
+            Stream output = response.OutputStream;
+            Utf8JsonWriter writer = new Utf8JsonWriter(output);
+            SampleData data = new SampleData
+            {
+                Id = 0,
+                UserName = "Arnaldo"
+            };
+            JsonSerializer.Serialize<SampleData>(writer, data, jsonSerializerOptions);
+            output.Close();
+        }
+
+        static void SendNotFound(HttpListenerResponse response)
+        {
+            // TODO: Error handling and finally clause for closing output stream
+            response.StatusCode = 404;
+            Stream output = response.OutputStream;
+            byte[] buffer = Encoding.UTF8.GetBytes("Not found.");
+            output.Write(buffer, 0, buffer.Length);
+            output.Close();
+        }
+    }
+
+    class SampleData
+    {
+        public Int32 Id { get; set; }
+
+        public String UserName { get; set; }
+    }
+}
